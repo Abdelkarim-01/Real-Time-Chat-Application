90638790549b60a42b1ee7a37a9dc1a030a5e1ec
Idrissi <idrissiabdelkarim01@gmail.com>
2024-03-29 22:54:43 +0000

    Initialize backend

---
+++ SpringServerSide/src/main/java/com/idrissichatapp/springserverside/SpringServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.idrissichatapp.springserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerSideApplication.class, args);
+    }
+
+}
---
+++ SpringServerSide/src/test/java/com/idrissichatapp/springserverside/SpringServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idrissichatapp.springserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
